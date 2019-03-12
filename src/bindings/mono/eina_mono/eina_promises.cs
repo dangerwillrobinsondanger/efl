886354fa61d87f58d99cf4d1a27d8ab1c1bca67d
Lauro Moura <lauromoura@expertisesolutions.com.br>
2019-03-12 17:21:15 -0300

    csharp: Uncomment future chaining.
    This is required for future cancelling tests.

--- src/bindings/mono/eina_mono/eina_promises.cs
+++ src/bindings/mono/eina_mono/eina_promises.cs
@@ -198,10 +198,10 @@ public class Future
     public Future(IntPtr handle)
     {
         Handle = handle;
-        // Handle = ThenRaw(handle, (Eina.Value value) => {
-        //     Handle = IntPtr.Zero;
-        //     return value;
-        // });
+        Handle = ThenRaw(handle, (Eina.Value value) => {
+            Handle = IntPtr.Zero;
+            return value;
+        });
     }
 
     /// <summary>
