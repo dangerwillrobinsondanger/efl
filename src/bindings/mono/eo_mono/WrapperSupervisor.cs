6aeb1c4659a43ecae5d197a5a4206d0425a19417
Vitor Sousa <vitorsousa@expertisesolutions.com.br>
2019-05-15 19:50:58 -0300

    csharp: pin-unpin lifetime (NEEDS REVIEW)

---
+++ src/bindings/mono/eo_mono/WrapperSupervisor.cs
@@ -0,0 +1,60 @@
+using System;
+using EventDictionary = System.Collections.Generic.Dictionary<(System.IntPtr desc, object evtDelegate), (System.IntPtr evtCallerPtr, Efl.EventCb evtCaller)>;
+
+namespace Efl
+{
+
+namespace Eo
+{
+
+/// <summary>Observe the ownership state  of an Eo wrapper and control its life-cycle.</summary>
+public class WrapperSupervisor
+{
+    private System.WeakReference weakRef;
+    private Efl.Eo.IWrapper sharedRef;
+    private EventDictionary eoEvents;
+
+    /// <summary>Create a new supervisor for the given.</summary>
+    /// <param name="eo">Efl object to be supervised.</param>
+    public WrapperSupervisor(Efl.Eo.IWrapper obj)
+    {
+        weakRef = new WeakReference(obj);
+        sharedRef = null;
+        eoEvents = new EventDictionary();
+    }
+
+    /// <summary>Efl object being supervised.</summary>
+    public Efl.Eo.IWrapper Target
+    {
+        get
+        {
+            return (Efl.Eo.IWrapper) weakRef.Target;
+        }
+    }
+
+    /// <summary>Dictionary that holds the events related with the supervised object.</summary>
+    public EventDictionary EoEvents
+    {
+        get
+        {
+            return eoEvents;
+        }
+    }
+
+    /// <summary>To be called when the object is uniquely owned by C#, removing its strong reference and making it available to garbage collection.</summary>
+    public void MakeUnique()
+    {
+        sharedRef = null;
+    }
+
+    /// <summary>To be called when the object is owned in the native library too, adding a strong reference to it and making it unavailable for garbage collection.</summary>
+    public void MakeShared()
+    {
+        sharedRef = this.Target;
+    }
+}
+
+}
+
+}
+
--- src/tests/efl_mono/Inheritance.cs
+++ src/tests/efl_mono/Inheritance.cs
@@ -10,6 +10,11 @@ using static EinaTestData.BaseData;
 namespace TestSuite
 {
 
+internal class Rubbish
+{
+    public int Content { get; set; } = 0;
+}
+
 class TestInheritance
 {
     internal class Inherit1 : Dummy.TestObject
@@ -34,7 +39,45 @@ class TestInheritance
             return "Hello World";
         }
     }
-    
+
+    internal class Inherit3Parent : Dummy.TestObject
+    {
+        public bool disposed = false;
+        public bool childDisposed = false;
+
+        ~Inherit3Parent()
+        {
+            Console.WriteLine("finalizer called for parent");
+        }
+
+        protected override void Dispose (bool disposing)
+        {
+            Console.WriteLine("Dispose parent");
+            base.Dispose(disposing);
+        }
+    }
+
+    internal class Inherit3Child : Dummy.TestObject
+    {
+        Inherit3Parent parent;
+        public Inherit3Child (Inherit3Parent parent) : base (parent)
+        {
+            this.parent = parent;
+        }
+
+        ~Inherit3Child()
+        {
+            Console.WriteLine("finalizer called for child");
+        }
+
+        protected override void Dispose (bool disposing)
+        {
+            parent.childDisposed = true;
+            Console.WriteLine("Dispose parent");
+            base.Dispose(disposing);
+        }
+    }
+
     public static void test_inherit_from_regular_class()
     {
         var obj = new Inherit1();
@@ -49,6 +92,101 @@ class TestInheritance
         Test.AssertEquals (50, i);
         string s = Dummy.InheritHelper.ReceiveDummyAndCallInStringshare(obj);
         Test.AssertEquals ("Hello World", s);
+    }
+
+    private static void MakeRubbish(out WeakReference objWRef)
+    {
+        var obj = new Inherit3Parent();
+
+        objWRef = new WeakReference(obj);
+
+
+        int n = 2111000;
+        var arr1 = new Rubbish[n];
+        var arr2 = new Rubbish[n];
+        var rand = new Random();
+        for (int i = 0; i != n; ++i)
+        {
+            arr1[i] = new Rubbish();
+            arr1[i].Content = rand.Next();
+        }
+
+        for (int i = n-1; i != -1; --i)
+        {
+            arr2[i] = new Rubbish();
+            arr2[i].Content = arr1[(i+n/2)%n].Content + (arr1[i].Content%2 == 0 ? 1 : -1);
+        }
+
+        var x = rand.Next(n);
+        Console.WriteLine($"arr1[x] = {arr1[x].Content}; arr2[x] = {arr2[x].Content}");
+    }
+
+    private static void hsasdgsdahd(out WeakReference parentWRef, out WeakReference childWRef)
+    {
+        //var parent = new Inherit3Parent();
+        //var child = new Inherit3Child(parent);
+
+        var parent = new Dummy.TestObject();
+        var child = new Dummy.TestObject(parent);
+
+        parentWRef = new WeakReference(parent);
+        childWRef = new WeakReference(child);
+
+        Console.WriteLine($"MMMMM Parent [{parent.ToString()}] has {Efl.Eo.Globals.efl_ref_count(parent.NativeHandle)} refs");
+        Console.WriteLine($"MMMMM Child [{child.ToString()}] has {Efl.Eo.Globals.efl_ref_count(child.NativeHandle)} refs");
+
+        child = null;
+
+        System.GC.Collect(System.GC.MaxGeneration, GCCollectionMode.Forced, true, true);
+        System.GC.WaitForPendingFinalizers();
+        Efl.App.AppMain.Iterate();
+
+        child = (Dummy.TestObject) childWRef.Target;
+
+        Test.AssertNotNull(parent);
+        Test.AssertNotNull(child);
+        //Test.AssertEquals(false, parent.disposed);
+        //Test.AssertEquals(false, parent.childDisposed);
+
+        Console.WriteLine($"MMMMM Parent [{parent.ToString()}] has {Efl.Eo.Globals.efl_ref_count(parent.NativeHandle)} refs");
+        Console.WriteLine($"MMMMM Child [{child.ToString()}] has {Efl.Eo.Globals.efl_ref_count(child.NativeHandle)} refs");
+
+        parent = null;
+        child = null;
+    }
+
+    public static void test_inherit_lifetime()
+    {
+        WeakReference parentWRef;
+        WeakReference childWRef;
+
+        System.GC.Collect();
+        System.GC.WaitForPendingFinalizers();
+        Efl.App.AppMain.Iterate();
+
+        hsasdgsdahd(out parentWRef, out childWRef);
+
+        WeakReference objWRef;
+        MakeRubbish(out objWRef);
+
+        for (int i = 0; i < 10; ++i)
+        {
+            for (int j = 0; j < 1000; ++j)
+            {
+                System.GC.Collect();
+            }
+
+            System.GC.WaitForPendingFinalizers();
+            Efl.App.AppMain.Iterate();
+        }
+
+        var obj = (Rubbish) objWRef.Target;
+        var parent = (Dummy.TestObject) parentWRef.Target;
+        var child = (Dummy.TestObject) childWRef.Target;
+
+        Test.AssertNull(obj);
+        Test.AssertNull(parent);
+        Test.AssertNull(child);
     }
 }
 
